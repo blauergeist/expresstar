5df8522c266658e08cc98e6e29f6577ad64543e9
blauergeist <banhammer.cinema@gmail.com>
2019-03-22 05:32:33 +0100

    Added Profile model

---
+++ models/Profile.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Creating schema
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 32
+  },
+  location: [
+    {
+      city: {
+        type: String,
+        required: true,
+        max: 32
+      },
+      street: {
+        type: String,
+        required: true,
+        max: 32
+      },
+      zipcode: {
+        type: String,
+        required: true,
+        max: 16
+      },
+      phone: {
+        type: String,
+        required: true,
+        max: 32
+      }
+    }
+  ],
+  company: {
+    type: String,
+    max: 32
+  },
+  description: {
+    type: String
+  },
+  social: {
+    olx: {
+      type: String
+    },
+    facebook: {
+      type: Facebook
+    },
+    instagram: {
+      type: String
+    }
+  },
+  website: {
+    type: String
+  }
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
---
+++ validation/register.js
@@ -0,0 +1,51 @@
+const Validator = require("validator");
+const isEmpty = require("./is-Empty");
+
+module.exports = function validateRegisterInput(data) {
+  let errors = {};
+
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.name = !isEmpty(data.name) ? data.name : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+
+  //name validation
+  if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
+    errors.name = "Name must be between 2 and 30 characters";
+  }
+
+  if (Validator.isEmpty(data.name)) {
+    errors.name = "Name field is required";
+  }
+
+  //email validation
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  }
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+
+  //password validation
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+
+  if (!Validator.isLength(data.password, { min: 8, max: 20 })) {
+    errors.password = "Password must consist of minimum 8 characters";
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password2 = "Confirm Password field is required";
+  }
+
+  if (!Validator.equals(data.password2, data.password)) {
+    errors.password2 = "Password don't match";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
