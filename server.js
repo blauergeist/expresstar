0b873d6e910e781c5e4be8ac488e577d7559fac8
blauergeist <banhammer.cinema@gmail.com>
2019-05-10 05:55:40 +0200

    Fixed syntax errors.

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use("/api/users", users);
 //static assets for production
 if (process.env.NODE_ENV === "production") {
   //set a static folder
-  app.use(express.static(client / build));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
